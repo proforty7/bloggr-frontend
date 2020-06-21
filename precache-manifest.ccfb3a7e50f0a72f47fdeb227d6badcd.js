e064cc71b91032025869dbd80a82c29a5c24f1e1
proforty7 <proforty7@gmail.com>
2020-06-21 07:42:46 +0530

    Updates

---
+++ precache-manifest.ccfb3a7e50f0a72f47fdeb227d6badcd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b06aed2827a80c4d6f6b7b02928af0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4dfee7913f354325ddf",
+    "url": "/static/css/2.c1463059.chunk.css"
+  },
+  {
+    "revision": "5a14efb43fe6e9a60701",
+    "url": "/static/css/main.e4fee53a.chunk.css"
+  },
+  {
+    "revision": "b4dfee7913f354325ddf",
+    "url": "/static/js/2.0595612e.chunk.js"
+  },
+  {
+    "revision": "022ecde5279ac92e262624afe12dc6c1",
+    "url": "/static/js/2.0595612e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a14efb43fe6e9a60701",
+    "url": "/static/js/main.3d5d6d0d.chunk.js"
+  },
+  {
+    "revision": "6bd8200d2a4d6857e9d7",
+    "url": "/static/js/runtime-main.cebe0f4b.js"
+  },
+  {
+    "revision": "e642fe0f5d3b8e34491edb7b1ebb209a",
+    "url": "/static/media/create-profile-bg.e642fe0f.jpg"
+  },
+  {
+    "revision": "a8e9e3b86632667be3d739b9ef7b85cc",
+    "url": "/static/media/dashboard-bg.a8e9e3b8.jpg"
+  },
+  {
+    "revision": "15413a89ded24bd12f4f2c258886b0b2",
+    "url": "/static/media/hero-bg.15413a89.jpg"
+  },
+  {
+    "revision": "9daa32a3339a78c8405196fe80cd6932",
+    "url": "/static/media/logo.9daa32a3.png"
+  },
+  {
+    "revision": "43491a59f1a54b7ac1dad76abf1975fd",
+    "url": "/static/media/signin-bg.43491a59.jpg"
+  },
+  {
+    "revision": "cd5ba73fa94af91e9bef430cbcb4fb27",
+    "url": "/static/media/signup1.cd5ba73f.jpg"
+  }
+]);
